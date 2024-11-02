5aca7d795a317cd6f6f45f41405b8d77804c456c
Bigi-Med <medbigi2000@gmail.com>
2024-11-02 14:54:55 +0100

    repeat xor

--- hexToBase64.go
+++ hexToBase64.go
@@ -21,10 +21,23 @@ func main(){
     //text,key,_ := xorWithAllBytes("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
     //fmt.Printf(text)
     //fmt.Printf(string(key))
-    path := "decode.txt"
-    lines := readFile(path)
-    text := detectSingleCharXor(lines)
-    fmt.Printf(text)
+   // path := "decode.txt"
+   // lines := readFile(path)
+   // text := detectSingleCharXor(lines)
+   // fmt.Printf(text)
+   repeatMsg1 := "Burning 'em, if you ain't quick and nimble"
+   repeatMsg2 := "I go crazy when I hear a cymbal"
+   key := "ICE"
+   brepeatMsg1 := convertStringToByteSlice(repeatMsg1)
+   brepeatMsg2 := convertStringToByteSlice(repeatMsg2)
+   bkey := convertStringToByteSlice(key)
+   bcoded1 := repeatingKey(brepeatMsg1,bkey)
+   bcoded2 := repeatingKey(brepeatMsg2,bkey)
+   coded1 := convertByteHex(bcoded1)
+   coded2 := convertByteHex(bcoded2)
+   coded := coded1+coded2
+   fmt.Printf(coded)
+
 
 }
 
@@ -104,7 +117,26 @@ func detectSingleCharXor(line []string) string {
    return bestText
 }
 
-
+func repeatingKey(bstr,bkey []byte) []byte{
+    var bcode []byte
+    var index int
+    for i:=0; i<len(bstr);i++{
+        bcode = append(bcode,bstr[i]^bkey[index] )
+        index++
+        if index == 3 {
+            index = 0
+        }
+    } 
+    return bcode
+}
+func convertStringToByteSlice(str string) []byte{
+    bstr := []byte(str)
+    return bstr
+}
+func convertByteHex(str []byte) string{
+  hex := hex.EncodeToString(str)   
+  return hex
+}
 
 
 
